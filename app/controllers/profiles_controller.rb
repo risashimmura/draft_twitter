56136412aecbecd0b64071eb9df7cef2791a0526
Risa Shimmura <risashimmura22@gmail.com>
2020-07-12 13:32:55 -0700

    profile form is functional and we link properly from new_profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,5 +4,17 @@ class ProfilesController < ApplicationController
 	def new 
 		@profile = Profile.new
 
+
+		@profile = Profile.new(profile_params)
+		# @profile.user = current_user
+		if @profile.save
+			flash[:success] = "Success! Your profile has been created."
+			redirect_to
+		else
+			p @profile.errors.inspect
+			render 'new'
 	end
 end
+
+def profile_params
+	params.require(:profile).permit(:first_name, :last_name, :age)
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,19 @@
 class StaticPagesController < ApplicationController
 	def main_page
-		@mission_statement = "Start tweeting!"
+		if user_signed_in?
+			if current_user.profile
+				redirect_to new_tweet_path
+			else
+				redirect_to new_profile_path
+			end
+
+		else 
+			@mission_statement = "Start tweeting!"
+		end
 	end
+
+	def about_us
+
+	end
+
 end
